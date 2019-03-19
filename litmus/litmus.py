daa9e021ae5a22cfb45edf1c05bcf66b006a6b92
Litmus-ModenJay <litmus.modejay@gmail.com>
2019-03-19 22:47:37 +0900

    Litmus.db update

--- litmus/litmus.py
+++ litmus/litmus.py
@@ -2,28 +2,6 @@ import os
 import json
 import math
 
-<<<<<<< HEAD
-class LitmusDB():
-    
-    def __init__(self):
-        self.db = self.initialize()
-    
-    def initialize(self):
-        with open("litmus/litmus.json") as f:
-            dj = json.loads(f.read())
-        data = []
-        for item in dj['Default']:
-            name=item['name']
-            hexa=item['index']
-            data.append({'name':name, 'hexa':hexa})
-        return data
-    def all(self):
-        return self.db
-    def count(self):
-        return len(self.db)
-
-
-=======
 class Litmus():
     data = []
     @classmethod
